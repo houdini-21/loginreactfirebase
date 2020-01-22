87b789fd12881cfa26e50d77ed5597df038827b7
azylum-21 <marinerocruzfer@gmail.com>
2020-01-22 11:11:38 -0600

    estableciendo informacion de usuario en firebase y leendo informacion de usuario desde localstorage y firebase

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import FormularioSignin from "./FormularioSignin";
 import FormularioSignup from "./FormularioSignup";
+import Swal from "sweetalert2";
 import Home from "./Home";
 import * as firebase from "firebase/app";
 import "firebase/auth";
@@ -11,18 +12,49 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 console.log(firebaseApp);
 
 export default class Router extends Component {
+  state = {
+    Login: false,
+    Username: null
+  };
+
   signupPost = postSignup => {
     console.log(postSignup);
+    const name = postSignup.username
+    const mail = postSignup.email
+    const pass = postSignup.password
+
     firebase
       .auth()
-      .createUserWithEmailAndPassword(postSignup.email, postSignup.password)
+      .createUserWithEmailAndPassword(mail, pass)
 
       .then(user => {
         console.log("registrado", user);
+        Swal.fire({
+          icon: "success",
+          title: "Usuario Registrado",
+          text: name
+        });
+
+        const User = firebase.auth().currentUser;
+
+        User.updateProfile({
+          displayName: name,
+          photoURL: "https://example.com/jane-q-user/profile.jpg"
+        })
+        this.setState({
+          Login: true,
+          Username: name
+        });
+        localStorage.setItem("User", JSON.stringify(this.state));
       })
+
       .catch(error => {
         var errorMessage = error.message;
-        alert(errorMessage);
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: errorMessage
+        });
       });
   };
 
@@ -30,12 +62,29 @@ export default class Router extends Component {
     firebase
       .auth()
       .signInWithEmailAndPassword(postSignin.email, postSignin.pass)
+  
       .then(user => {
         console.log("loggeado", user);
+        Swal.fire({
+          icon: "success",
+          title: "Bienvenido"
+        });
+            let userr = firebase.auth().currentUser;
+      let namee
+      if (userr != null) {
+        namee = userr.displayName;
+      }
+     const resul = JSON.parse(localStorage.getItem("User"));
+      const na = resul.Username
+     console.log(na)
       })
       .catch(error => {
         var errorMessage = error.message;
-        alert(errorMessage);
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: errorMessage
+        });
       });
 
     console.log(postSignin.email);
