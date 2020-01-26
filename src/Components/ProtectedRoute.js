7c07c43af503e90664f1a949804ed74923327c87
azylum-21 <marinerocruzfer@gmail.com>
2020-01-26 14:12:11 -0600

    I'm Inevitable, rutas funcionando y protegidas

---
+++ src/Components/ProtectedRoute.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+const key = localStorage.getItem("authToken");
+const ProtectedRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      key ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/",
+            state: { from: props.location }
+          }}
+        />
+      )
+    }
+  />
+);
+export default ProtectedRoute;
